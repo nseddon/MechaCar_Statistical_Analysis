817c46a0e8366348536f1a28d357bf23c91b51cc
Nick Seddon <nickseddon187@gmail.com>
2021-12-11 13:31:07 -0500

    Deliverable 2 added

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,17 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #Deliverable 1-6
 # Generate the summary statistics from the linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_df)) 
+
+#Deliverable 2-2
+#Read csv into a table
+coil_df <- read.csv(file='csv/Suspension_Coil.csv', check.names=F,stringsAsFactors = F)
+
+#Deliverable 2-3
+#Create total_summary through use of summarize function
+total_summary <- coil_df %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),PSI_Variance=var(PSI),Standard_Deviation=sd(PSI), .groups = 'keep')
+
+#Deliverable 2-4
+#Create total_summary through use of summarize function
+lot_summary <- coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),PSI_Variance=var(PSI),Standard_Deviation=sd(PSI), .groups = 'keep')
+
+
